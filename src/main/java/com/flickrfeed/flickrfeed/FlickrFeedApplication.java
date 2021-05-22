415716e513aa73415437095d593f561b40bfb647
sanusiharun <39834005+sanusiharun@users.noreply.github.com>
2021-05-22 09:42:39 +0700

    Add java source

---
+++ src/main/java/com/flickrfeed/flickrfeed/FlickrFeedApplication.java
@@ -0,0 +1,13 @@
+package com.flickrfeed.flickrfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickrFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickrFeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flickrfeed/flickrfeed/FlickrFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flickrfeed.flickrfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlickrFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
